ad04a5bbf660f62206fd5833ddf289f562fd6b41
Mohammad Taj <peakimo@gmail.com>
2024-09-07 20:25:40 +0330

    Add command [/Report]

--- src/main/java/org/peakimo/reportplayer/Commands/Report.java
+++ src/main/java/org/peakimo/reportplayer/Commands/Report.java
@@ -1,4 +1,48 @@
 package org.peakimo.reportplayer.Commands;
 
-public class Report {
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class Report implements CommandExecutor {
+
+    @Override
+    public boolean onCommand (CommandSender sender, Command command, String label, String[] args){
+
+        Player bazikon = (Player) sender;
+        String playername = bazikon.getName();
+
+        if (args.length > 0) {
+            StringBuilder message = new StringBuilder();
+            for (String arg : args) {
+                message.append(arg).append(" ");
+            }
+            String finalMessage = message.toString().trim();
+
+            //Ersal message to Admin report
+            for (Player onlineplayers : Bukkit.getOnlinePlayers()){
+
+                if (onlineplayers.hasPermission("ReportPlayer.admin")) {
+                    onlineplayers.sendMessage(color("&7[&c&lReport&r&7] " + playername + " &c| &f" + finalMessage));
+                }
+            }
+        }
+
+
+
+
+
+
+
+
+        return true;
+    }
+    private String color(String str) {
+        return ChatColor.translateAlternateColorCodes('&', str);
+
+
+    }
 }
--- src/main/java/org/peakimo/reportplayer/ReportPlayer.java
+++ src/main/java/org/peakimo/reportplayer/ReportPlayer.java
@@ -1,12 +1,14 @@
 package org.peakimo.reportplayer;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.peakimo.reportplayer.Commands.Report;
 
 public final class ReportPlayer extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        getCommand("report").setExecutor(new Report());
 
     }
 
